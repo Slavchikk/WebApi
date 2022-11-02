de77a6ed7459ebcd664bf4292e9738b349a4a866
КарзиновВА <@105-09>
2022-11-02 17:53:31 +0300

    Добавьте файлы проекта.

---
+++ WebApplication1/Controllers/CarsController.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using WebApplication1;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class CarsController : ApiController
+    {
+        private Entities db = new Entities();
+
+        // GET: api/Cars
+        [ResponseType(typeof(List<CarsModel>))]
+        public IHttpActionResult GetCars()
+        {
+            return Ok(db.Cars.ToList().ConvertAll(x => new CarsModel(x)));
+        }
+
+        // GET: api/Cars/5
+        [ResponseType(typeof(Cars))]
+        public IHttpActionResult GetCars(int id)
+        {
+            Cars cars = db.Cars.Find(id);
+            if (cars == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(cars);
+        }
+
+        // PUT: api/Cars/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutCars(int id, Cars cars)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != cars.id_cars)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(cars).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CarsExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Cars
+        [ResponseType(typeof(Cars))]
+        public IHttpActionResult PostCars(Cars cars)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Cars.Add(cars);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = cars.id_cars }, cars);
+        }
+
+        // DELETE: api/Cars/5
+        [ResponseType(typeof(Cars))]
+        public IHttpActionResult DeleteCars(int id)
+        {
+            Cars cars = db.Cars.Find(id);
+            if (cars == null)
+            {
+                return NotFound();
+            }
+
+            db.Cars.Remove(cars);
+            db.SaveChanges();
+
+            return Ok(cars);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool CarsExists(int id)
+        {
+            return db.Cars.Count(e => e.id_cars == id) > 0;
+        }
+    }
+}
---
+++ WebApplication1/Models/CarsModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class CarsModel
+    {
+        public CarsModel(Cars cars)
+        {
+            id_cars = cars.id_cars;
+            carsModel = cars.CarsModel;
+            carsBrand = cars.CarsBrand;
+            price = cars.Price;
+            image = cars.image;
+        }
+
+        public int id_cars { get; set; }
+        public string carsBrand { get; set; }
+        public string carsModel { get; set; }
+        public int price { get; set; }
+        public string image { get; set; }
+
+    }
+}
